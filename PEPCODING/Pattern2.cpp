a84433119b6bab917f82738651ebd4c23eea5515
Kritika Tripathi <kritika.tripathi03@gmail.com>
2022-03-26 13:53:55 +0530

    Added file Pattern1.cpp

---
+++ PEPCODING/Pattern2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    
+    //write your code here
+    for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                cout<<"*"<<"\t";
+            }
+            cout<<endl;
+        }
+}
