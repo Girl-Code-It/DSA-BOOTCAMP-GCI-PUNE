73f96059edc8f27f16d7ab2ad2c892e85fff9d91
tanisha-tech360 <tanishagupta1234.college@gmail.com>
2022-03-26 21:46:28 +0530

    Pepcoding level 1 done till( print all prime number till n)

---
+++ Tanisha/Basicsofprogramming/Gettingstarted/Is_a_number_prime.java
@@ -0,0 +1,27 @@
+package Tanisha.Basicsofprogramming.Gettingstarted;
+import java.util.*;
+public class Is_a_number_prime {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for (int i = 0;i<n;i++) {
+            prime(scn.nextInt());
+        }
+        scn.close();
+    }
+
+    public static void prime(int n) {
+        int t=0;
+        for(int i=2;i<=n/2;i++) {
+            if(n%i==0) {
+                t=1;
+                break;
+            }
+        }
+        if(t==0) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
+}
