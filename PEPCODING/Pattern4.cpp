d1f4e3bf8be0a8b144e0b361d8af53ff319dea47
Kritika Tripathi <kritika.tripathi03@gmail.com>
2022-03-26 14:05:39 +0530

    added file Pattern3.cpp

---
+++ PEPCODING/Pattern4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    //write your code here
+    for(int i=n; i>=1; i--){
+        for(int k=1; k<=n-i;k++){
+            cout<<"\t";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<"*"<<"\t";
+        }
+        cout<<endl;
+    }
+
+}
