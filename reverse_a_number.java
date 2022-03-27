f375bbb75f4c3353e9171cb7b138ae8cd30ffc62
tanisha-tech360 <tanishagupta1234.college@gmail.com>
2022-03-27 21:20:48 +0530

    Pepcoding(level 1) till reverse a number

---
+++ reverse_a_number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class digits_of_a_number {
+    public static void main(String[] args){
+     
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        char[] ch = String.valueOf(n).toCharArray();
+        for (int i = ch.length-1; i>=0; i--) {
+            System.out.println(ch[i]);
+        }
+        scn.close();
+    }
+}
+
+
