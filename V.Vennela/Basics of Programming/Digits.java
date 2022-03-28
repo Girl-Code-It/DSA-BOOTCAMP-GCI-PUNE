b2fcb4fbe1d47cc32f4457b066a94443aac431e6
Vangapally-Vennela <94056568+Vangapally-Vennela@users.noreply.github.com>
2022-03-28 21:57:40 +0530

    Create Digits.java

---
+++ V.Vennela/Basics of Programming/Digits.java
@@ -0,0 +1,18 @@
+import java.util.*;
+    
+    public class Main{
+    public static void digit(long n)
+    {
+        if((int)n==0)
+            return ;
+        digit(n/10);
+        System.out.println(n%10);
+    }
+    
+    public static void main(String[] args) {
+      // write your code here  
+    Scanner sc = new Scanner(System.in);
+    long n = sc.nextLong();
+    digit(n);
+   }
+  }
