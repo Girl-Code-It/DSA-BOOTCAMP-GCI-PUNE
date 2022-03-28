e437e382fac8886e6d9de60831641c01e80c437a
Muskan <59757988+Muskan-creator@users.noreply.github.com>
2022-03-28 09:14:24 +0530

    Create Pattern5.cpp

---
+++ Muskan/Basics of programming/Patterns/Pattern5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+   int stars=1;
+   int spaces=n/2;
+   for(int i=1;i<=n;i++){
+       for(int j=1;j<=spaces;j++){
+           cout<<"\t";
+       }
+        for(int j=1;j<=stars;j++){
+           cout<<"*\t";
+       }
+       cout<<endl;
+       if(i<=n/2){
+           spaces--;
+           stars+=2;
+       }
+       else{
+           spaces++;
+           stars-=2;
+       }
+   }
+
+    
+}
