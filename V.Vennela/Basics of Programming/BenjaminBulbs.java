289dd12ef08518483494fc76165572dd03b23283
Vangapally-Vennela <94056568+Vangapally-Vennela@users.noreply.github.com>
2022-03-29 23:13:13 +0530

    Create BenjaminBulbs.java

---
+++ V.Vennela/Basics of Programming/BenjaminBulbs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+  
+  public class BenjaminBulbs{
+  
+  public static void main(String[] args) {
+    // write your code here  
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i=1;i*i<=n;i++)
+    {
+        System.out.println(i*i);
+    }
+   }
+  }
