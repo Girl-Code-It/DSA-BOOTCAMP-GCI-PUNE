4e9764de2c5f9bb2b4660be8faddfd77a72211ba
Umme Rubaiyat Chowdhury <ummerubaiyat@gmail.com>
2022-03-30 16:44:20 +0600

    Print z (Basics of Programming)

---
+++ Umme_Rubaiyat/Basics of Programming/problem_1/print_z.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+//1. You are required to print a 'z' of size 5 using '*'.
+using namespace std;
+
+int main()
+{
+    cout<<"*****\n";
+    cout<<"   *\n";
+    cout<<"  *\n";
+    cout<<" *\n";
+    cout<<"*****\n";
+
+    return 0;
+}
