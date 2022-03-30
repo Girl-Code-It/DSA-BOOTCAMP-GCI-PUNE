5e272a825d45edd0b3155cd11798d1a5db7d96f1
Vangapally-Vennela <94056568+Vangapally-Vennela@users.noreply.github.com>
2022-03-31 01:06:35 +0530

    Create Pattern9.java

---
+++ V.Vennela/Basics of Programming/Patterns/Pattern9.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Pattern9 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==j || i+j==n+1)
+                    System.out.print("*\t");
+                else
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
