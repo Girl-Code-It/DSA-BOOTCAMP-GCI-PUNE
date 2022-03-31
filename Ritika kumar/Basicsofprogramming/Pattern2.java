2358b84c79895e968b88e1ab55eb940039cf4730
Ritika Kumar <kumarritika666@gmail.com>
2022-03-31 10:16:13 -0700

    create pattern2

---
+++ Ritika kumar/Basicsofprogramming/Pattern2.java
@@ -0,0 +1,14 @@
+public class Pattern2 {
+    public static void main(String[]args)
+    {
+        for(int i=0 ;i<5; i++)
+        {
+            for(int j= 5  ;j> 0 ;j--)
+            {
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
