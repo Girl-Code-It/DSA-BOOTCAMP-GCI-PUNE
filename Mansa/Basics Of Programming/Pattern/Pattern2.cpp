10b3237f8c4a48129aaa530310d31957d0e2f5d3
Mansa Dixit <mansadixit123@gmail.com>
2022-04-01 00:35:36 +0530

    Create Pattern2.cpp

---
+++ Mansa/Basics Of Programming/Pattern/Pattern2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for (i = 5; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
