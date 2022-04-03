762ad0e5508f17cba1f837a21da66ce2ea1b96e9
Kritika Tripathi <kritika.tripathi03@gmail.com>
2022-04-03 14:00:02 +0530

    Completed Pattern Exercise

---
+++ Kritika Tripathi/Basics Of Programming/Getting Started/Grading_System.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cin>>marks;
+    
+    //write your code here
+    if(marks>90){
+        cout<<"Excellent!";
+    }
+    else if(marks>80 && marks<=90){
+        cout<<"Good!";
+    }
+    else if(marks>70 && marks<=80){
+        cout<<"Fair!";
+    }
+    else if(marks>60 && marks<=70){
+        cout<<"Meets Expectations!";
+    }
+    else if(marks<=60){
+        cout<<"Below par!";
+    }
+}
