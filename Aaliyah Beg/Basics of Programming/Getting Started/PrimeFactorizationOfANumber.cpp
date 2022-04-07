6da44d420db4a217bf58a7cd0a0fee1085eee12e
Aaliyah Beg <aaliyahbeg123@gmail.com>
2022-04-07 12:12:44 +0530

    add prime fac ques

--- Aaliyah Beg/Basics of Programming/Getting Started/PrimeFactorizationOfANumber.cpp
+++ Aaliyah Beg/Basics of Programming/Getting Started/PrimeFactorizationOfANumber.cpp
@@ -1,22 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+//print prime factors
+// int main() {
+//     int n;cin>>n;
+//     bool isPrime[n];
+//     for(int i=0;i<n;i++) isPrime[i]=true;
+//     isPrime[1]=false;
+//     isPrime[0]=false;
+//     for(int i=2;i<n;i++){
+//         if(isPrime[i]==true){
+//             for(int j=2*i;j<n;j+=i){
+//                 isPrime[j]=false;
+//             }
+//         }
+//     }
+//     for(int i=2;i*i<n;i++) {
+//         if(n%i==0 and isPrime[i]){
+//             cout<<i<<"\n";
+//         }
+//     }
+//     return 0;
+// }
+//print prime factorization
+int main(){
     int n;cin>>n;
-    bool isPrime[n];
-    for(int i=0;i<n;i++) isPrime[i]=true;
-    isPrime[1]=false;
-    isPrime[0]=false;
-    for(int i=2;i<n;i++){
-        if(isPrime[i]==true){
-            for(int j=2*i;j<n;j+=i){
-                isPrime[j]=false;
+    int spf[n+1];
+    for(int i=2;i<=n;i++) spf[i]=-1;
+    for(int i=2;i<=n;i++){
+        if(spf[i]==-1){
+            spf[i]=i;
+            for(int j=i*i;j<=n;j+=i){
+                if(spf[j]==-1) spf[j]=i;
             }
         }
     }
-    for(int i=2;i*i<n;i++) {
-        if(n%i==0 and isPrime[i]){
-            cout<<i<<"\n";
-        }
+    while(n>1){
+        cout<<spf[n]<<" ";
+        n/=spf[n];
     }
-    return 0;
 }
