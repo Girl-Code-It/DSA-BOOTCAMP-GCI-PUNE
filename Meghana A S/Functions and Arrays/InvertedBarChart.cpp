a9da839c0a0f7d9d2c59084c79b2c87fe41ea493
Meghana Ayyala Somayajula <76214336+meghanaayyala@users.noreply.github.com>
2022-04-16 19:53:38 +0530

    Create InvertedBarChart.cpp

---
+++ Meghana A S/Functions and Arrays/InvertedBarChart.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+  //write your code here
+int n;
+    cin>>n;
+    int arr[n];
+    int max=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    int mat[max][n];
+    for(int i=0;i<max;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i<arr[j]){
+                cout <<"*	";
+            }
+            else{
+                cout<<"	";
+            }
+        }
+        cout<<endl;
+    }
+
+}
