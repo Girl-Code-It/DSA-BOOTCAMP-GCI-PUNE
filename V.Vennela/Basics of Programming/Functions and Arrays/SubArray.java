9843e95efd47e0349778ce6da71e1c6834f78add
Vangapally-Vennela <94056568+Vangapally-Vennela@users.noreply.github.com>
2022-04-16 16:29:47 +0530

    Create SubArray.java

---
+++ V.Vennela/Basics of Programming/Functions and Arrays/SubArray.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class SubArray{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int a[] = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        a[i]=sc.nextInt();
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            for(int k=i;k<=j;k++)
+            {
+                System.out.print(a[k]+"\t");
+            }
+            System.out.println();
+        }
+        
+    }
+ }
+
+}
