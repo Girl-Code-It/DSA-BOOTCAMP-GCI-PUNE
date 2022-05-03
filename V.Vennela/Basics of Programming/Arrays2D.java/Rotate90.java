c7413805ca0207baa0aac7ceb7bb9152882ef950
Vangapally-Vennela <94056568+Vangapally-Vennela@users.noreply.github.com>
2022-05-03 18:14:34 +0530

    Create Rotate90.java

---
+++ V.Vennela/Basics of Programming/Arrays2D.java/Rotate90.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Rotate90 {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[][] = new int[n][n];
+    
+    for(int i=0;i<n;i++)
+    {
+         for(int j=0;j<n;j++)
+        {
+            arr[i][j]=sc.nextInt();
+        }
+    }
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < i; j++) {
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+      }
+    }
+    for (int i = 0; i < arr.length; i++) {
+      int li = 0;
+      int ri = arr[0].length - 1;
+      while (li <= ri) {
+        int temp = arr[i][li];
+        arr[i][li] = arr[i][ri];
+        arr[i][ri] = temp;
+
+        li++;
+        ri--;
+      }
+    }
+
+    display(arr);
+        
+    }
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
